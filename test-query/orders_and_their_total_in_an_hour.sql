6424930700d891c2f6136a38cc7c37a7ff322bbc
caden-miller <cadenmiller40@gmail.com>
2024-02-21 00:41:28 -0600

    added junction table for c_order to items that also generates data.

--- test-query/orders_and_their_total_in_an_hour.sql
+++ test-query/orders_and_their_total_in_an_hour.sql
@@ -1,8 +1,4 @@
 SELECT COUNT(*) AS totalOrdersInHour, SUM(c_order_total) AS totalOrderCostInAnHour 
 FROM customer_order 
-<<<<<<< Updated upstream
-WHERE DATE_PART(‘hour’, c_order_time) = 5;
-=======
 
 WHERE DATE_PART('hour', c_order_time) = 5;
->>>>>>> Stashed changes
