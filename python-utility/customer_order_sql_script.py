a4fc019903d77c27e704b8f43cb45752be75d48d
Jesungha <113495115+Jesungha@users.noreply.github.com>
2024-02-20 22:42:18 -0600

    2 years now

--- python-utility/customer_order_sql_script.py
+++ python-utility/customer_order_sql_script.py
@@ -19,13 +19,25 @@ def printout():
     total_sales = 0
 
     i = 1
-    for month in range(2,13):
+    for month in range(2,26):
+        if month > 24:
+            year = "2024-"
+            month -= 24
+        elif month > 12:
+            year = "2023-"
+            month -= 12
+        elif month > 24:
+            year = "2024-"
+            month -= 24
+        else:
+            year = "2022-"
         if month in [1,3,5,7,8,10,12]:
             days = 31
         elif month == 2:
             days = 28
         else:
             days = 30
+        
         for day in range(1,days+1):
             day_sale = 0
             if month == 8 and day == 23:
@@ -35,10 +47,6 @@ def printout():
             else:
                 day_limit = 2750
             while day_sale < day_limit:
-                if month in [2,3,4,5,6,7,8,9,10,11,12]:
-                    year = "2023-"
-                else:
-                    year = "2024-"
                 f.write("INSERT INTO Customer_order (c_order_id, c_order_date, c_order_time, c_order_subtotal, c_order_tax, c_order_total, c_order_payment_type) VALUES (")
                 f.write(str(i)+", '"+ year+ str(month)+ "-" + str(day)+ "', '")
                 f.write(str(random.randrange(11,23))+ ":" + str(random.randrange(0,60))+ ":" + str(random.randrange(0,60))+ "', ") # 11am to pm
@@ -57,7 +65,8 @@ def printout():
                 payments = ["cash", "credit", "debit"]
                 f.write("'"+ payments[random.randrange(0,3)]+ "');\n")
                 i += 1
- 
+    print(total_sales)
+"""
     for day in range(1,32):
         day_sale = 0
         if day == 17:
@@ -85,6 +94,7 @@ def printout():
             i += 1
     print(total_sales)
     f.close()
+"""
 
 if __name__ == "__main__":
     printout()
