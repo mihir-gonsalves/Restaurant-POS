1ac5440107bff2b57f62460f281b3282c56318db
kJo75 <96453839+kJo75@users.noreply.github.com>
2024-02-21 00:07:35 -0600

    Added SQL file for menu item generation
    Uploaded SQL file with 28 inserts for each menu item.

---
+++ sql-script/populate_menu_items.sql
@@ -0,0 +1,34 @@
+CREATE TABLE menu_items (
+    item_id INT PRIMARY KEY,
+    item_name VARCHAR(50),
+    item_price DECIMAL(15,2)
+);
+
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (1, '3 Tender Entree', 4.99);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (2, '2 Corn Dog Value Meal', 4.99);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (3, '2 Hot Dog Value Meal', 4.99);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (4, 'Chicken Caesar Salad', 8.29);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (5, 'Yell BBQ Rib Sandwich', 7.99);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (6, '2 Chicken Bacon Ranch Wrap', 6.00);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (7, 'Single Chicken Bacon Ranch Wrap', 3.49);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (8, '2 Classic Chicken Wraps', 5.00);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (9, 'Single Classic Chicken Wrap', 2.99);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (10, 'Double Scoop Ice Cream', 3.29);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (11, 'Cookie Ice Cream Sundae', 4.69);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (12, 'Chocolate Aggie Shake', 4.49);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (13, 'Vanilla Aggie Shake', 4.49);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (14, 'Strawberry Aggie Shake', 4.49);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (15, 'Oreo Cookie Aggie Shake', 4.49);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (16, 'Root Beer Float', 5.49);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (17, 'Bacon Cheeseburger', 8.29);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (18, 'Black Bean Burger', 7.59);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (19, 'Cheeseburger', 6.89);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (20, 'Gig Em Patty Melt', 7.59);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (21, 'Classic Hamburger', 6.89);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (22, 'French Fries', 1.99);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (23, 'Aggie Chicken Club', 8.39);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (24, 'Revs Grilled Chicken Sandwich', 8.39);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (25, 'Spicy Chicken Sandwich', 8.39);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (26, 'Aquafina Water 16 OZ', 1.79);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (27, 'Aquafina Water 20 OZ', 2.19);
+INSERT INTO menu_items(item_id, item_name, item_price) VALUES (28, 'Pepsi Fountain 20 OZ', 1.99);
